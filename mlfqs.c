bc67d2925804fb7a187f371cb5970bc6cb070786
Lydia Parsa <lparsa1@lsu.edu>
2022-11-16 09:37:10 -0600

    Revert "prio algorithm doesnt go through if it is the null process"
    This reverts commit 9b9af65b3070df1b6021f76dc8da33b9d610d231.

--- mlfqs.c
+++ mlfqs.c
@@ -2,21 +2,47 @@
 #include <stdlib.h>
 #include "prioque.h"
 #include <string.h>
-#include <stdbool.h>
-#include "process.h"
+
+
+typedef struct Process
+{
+    int pid;
+    int arrival_time;
+    int CPU_time;
+    int savedTime;
+    int repeat;
+    int IOtime;
+    int savedIOTime;
+    int usage; //report usage
+    int priority;
+    int promote;
+    int demote;
+    int quantum;
+    Queue Behaviors;
+
+} Process;
+
+
+typedef struct ProcessBehavior
+{
+    unsigned long CPU_burst;
+    unsigned long IO_burst;
+    int repeat;
+
+} ProcessBehavior;
 
 // Queues
 Queue ArrivalQueue, HighQueue, MediumQueue, LowQueue, IOQueue;
 
 //variables
 int quantum = 0; //CPU given to processes in individual queues
-int result = NOTFINISHED; // 0 for doing IO, 1 for not finished, 2 for finished
-int CPU = 0; //model CPU clock
+int result = 1; // 0 for doing IO, 1 for not finished, 2 for finished
+int CPU; //model CPU clock
 char report[255];
 
 //special processes
 Process nullProcess; //null process
-Process *pointer;// points to null or the highest priority
+Process *pointer; // points to null or the highest priority
 Process preReadyProcess;
 Process removedProcess;
 
@@ -26,14 +52,13 @@ void priority_algorithm();
 void promote(Process *process);
 void demote(Process *process);
 void init_all_queues();
+void init_process(Process *process);
 void final_report();
 void preemption();
-bool processes_exist();
+int processes_exist();
 void input_output();
-int process_compare(const void *e1, const void *e2);
 int execute_process(Process *process);
 int execute_IO(Process *process);
-void init_process(Process *process);
 
 int process_compare(const void *e1, const void *e2) {
 	Process *p1 = (Process *)e1;
@@ -45,79 +70,8 @@ int process_compare(const void *e1, const void *e2) {
 	}
 }
 
-int execute_process(Process *process)
-{
-    process->usage++;
-    //why it is always two, because pid is always 0
-    if(process->pid != 0)
-    {
-
-        process->CPU_time--;
-        if(process->CPU_time == 0)
-        {
-
-            if(process->IOtime > 0)
-            {
-
-                process->CPU_time = process->savedTime;
-                return DOIO;
-                //do IO
-            }
-            else
-            {
-                return FINISHED;
-                //finished
-            }
-        }
-        else
-        {
-            return NOTFINISHED;
-            //not finished
-        }
-    }
-    else
-    {
-        return FINISHED;
-        //finished
-    }
-}
-
-int execute_IO(Process *process)
-{
-    process->IOtime--;
-    if(process->IOtime == 0)
-    {
-        process->repeat--;
-
-        if(process->repeat > 0)
-        {
-            process->IOtime = process->savedIOTime;
-        }
-        else
-        {
-            if(!empty_queue(&(process->Behaviors)))
-            {
-                ProcessBehavior behavior;
-                remove_from_front(&(process->Behaviors), &behavior);
-                process->CPU_time = behavior.CPU_burst;
-                process->savedTime = process->CPU_time;
-                process->IOtime = behavior.IO_burst;
-                process->savedIOTime = process->IOtime;
-                process->repeat = behavior.repeat;
-            }
-        }
-        return FINISHED;
-    }
-    else
-    {
-     return NOTFINISHED;
-    }
-}
-
 void read_process_descriptions(void)
 {
-    //might be weird
-    //bad if statement
     Process process;
     ProcessBehavior b;
     int pid;
@@ -139,8 +93,10 @@ void read_process_descriptions(void)
         process.arrival_time = arrival;
         first = 0;
         add_to_queue(&process.Behaviors, &b, 1);
+
     }
-    add_to_queue(&ArrivalQueue, &process, process.arrival_time);  
+    add_to_queue(&ArrivalQueue, &process, process.arrival_time);
+    
 }
 
 void init_all_queues()
@@ -175,20 +131,16 @@ void init_process(Process *process)
 
 void schedule_queues(Process *process)
 {
-    
     if(process->priority == 1)
     {
-        printf("priority is 1");
         add_to_queue(&HighQueue, process, process->quantum);
     }
     else if (process->priority == 2)
     {
-        printf("priority is 2");
         add_to_queue(&MediumQueue, process, process->quantum);
     }
     else if (process->priority == 3)
     {
-        printf("priority is 3");
         add_to_queue(&LowQueue, process, process->quantum);
     }
 }
@@ -210,7 +162,6 @@ void preemption()
         current.savedIOTime = behavior.IO_burst;
         current.repeat = behavior.repeat;
         schedule_queues(&current);
-        printf("PID: %d, ARRIVAL TIME: %d\n", current.pid, current.arrival_time);
         printf("CREATE: Process %d entered the ready queue at time %d\n", current.pid, CPU);
     }
 }
@@ -250,12 +201,14 @@ void demote(Process *process)
 
 void priority_algorithm()
 {
-if(pointer != &nullProcess){
-    //option 1, quantum at 0 (it used all of it) but process is not complete
+    //option 1, quantum at 0 but process is not complete
     if(quantum == 0)
     {
-        if(result == NOTFINISHED)
+        printf("quantum is 0");
+        if(result == 1)
         {
+            printf("result is 1");
+            printf("case 1: quantum is 0, result is 1");
             if(pointer->priority < 3)
             {
                 pointer->demote--;
@@ -274,8 +227,9 @@ if(pointer != &nullProcess){
         }
     }
     
-    if(result == DOIO)
+    if(result == 0)
     {
+        printf("case 2: result is 0");
         if(pointer->priority > 1)
         {
             pointer->promote--;
@@ -293,8 +247,9 @@ if(pointer != &nullProcess){
         pointer = &nullProcess;
     }
     //case 3, finished
-    else if(result == FINISHED)
+    else if(result == 2)
     {
+        printf("case 3: result is 2, finished");
         if(process_compare(&nullProcess, pointer))
         {
             char process_report[50];
@@ -323,7 +278,7 @@ if(pointer != &nullProcess){
             }
             removedProcess = preReadyProcess;
             pointer = &removedProcess;
-            quantum = pointer->quantum;
+            quantum= pointer->quantum;
             printf("RUN: Process %d started execution from level %d at time %d; wants to execute for %d ticks.\n", pointer->pid, pointer->priority, CPU, pointer->CPU_time);
         }
     }
@@ -350,21 +305,24 @@ if(pointer != &nullProcess){
                 pointer = &removedProcess;
                 quantum = pointer->quantum;
                 printf("RUN: Process %d started execution from level %d at time %d; wants to execute for %d ticks.\n", pointer->pid, pointer->priority, CPU, pointer->CPU_time);
+
          }
       }
     }
     result = execute_process(pointer);
     quantum--;
 }
-}
 
-bool processes_exist()
+int processes_exist()
 {
-    Process *pointer;
-    pointer = pointer_to_current(&ArrivalQueue);
-
-
-    return ((empty_queue(&HighQueue) && empty_queue(&LowQueue) && empty_queue(&MediumQueue))|| !empty_queue(&IOQueue) || !empty_queue(&ArrivalQueue) || process_compare(&nullProcess, pointer));
+    if ((empty_queue(&HighQueue) && empty_queue(&LowQueue) && empty_queue(&MediumQueue))|| !empty_queue(&IOQueue) || !empty_queue(&ArrivalQueue) || process_compare(&nullProcess, pointer))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
     //rn for some reason it is always true
 }
 
@@ -378,7 +336,7 @@ void input_output()
         {
             remove_from_front(&IOQueue, &inp);
             int result = execute_IO(&inp);
-            if (result != FINISHED)
+            if (result != 2)
             {
                 add_to_queue(&IOQueue, &inp, 1);
             }
@@ -387,6 +345,74 @@ void input_output()
                 schedule_queues(&inp);
             }
         }
+    }
+}
+
+int execute_process(Process *process)
+{
+    process->usage++;
+    if(process->pid != 0)
+    {
+
+        process->CPU_time--;
+        if(process->CPU_time == 0)
+        {
+
+            if(process->IOtime > 0)
+            {
+
+                process->CPU_time = process->savedTime;
+                return 0;
+                //do IO
+            }
+            else
+            {
+                return 2;
+                //finished
+            }
+        }
+        else
+        {
+            return 1;
+            //not finished
+        }
+    }
+    else
+    {
+        return 2;
+        //finished
+    }
+}
+
+int execute_IO(Process *process)
+{
+    process->IOtime--;
+    if(process->IOtime == 0)
+    {
+        process->repeat--;
+
+        if(process->repeat > 0)
+        {
+            process->IOtime = process->savedIOTime;
+        }
+        else
+        {
+            if(!empty_queue(&(process->Behaviors)))
+            {
+                ProcessBehavior behavior;
+                remove_from_front(&(process->Behaviors), &behavior);
+                process->CPU_time = behavior.CPU_burst;
+                process->savedTime = process->CPU_time;
+                process->IOtime = behavior.IO_burst;
+                process->savedIOTime = process->IOtime;
+                process->repeat = behavior.repeat;
+            }
+        }
+        return 2;
+    }
+    else
+    {
+     return 1;
     }
 }
 
@@ -402,23 +428,18 @@ int main(int argc, char *argv[])
 
     init_process(&removedProcess);
 
-    printf("Queue entries one per line:\n");
+    printf("Queue entries one per line:");
 
     read_process_descriptions();
 
-    while (processes_exist())
+    while (processes_exist() == TRUE)
     {
         CPU++;
-
-         printf("entering2\n");
         preemption();
-
-         printf("entering3\n");
         priority_algorithm();
-
-         printf("entering4\n");
         input_output();
     }
+    printf("line 418");
     CPU++;
     final_report();
     return 0;
